6006c78b6dbe050359badadf088160e8c1ae7b51
Nikolay <nikola19102@gmail.com>
2018-10-22 19:15:50 +0400

    fmc

--- sender.go
+++ sender.go
@@ -79,10 +79,8 @@ func (s *Sender) SendNoRetry(msg *Message) (*Response, error) {
 	}
 	req.Header.Add("Authorization", fmt.Sprintf("key=%s", s.ApiKey))
 	req.Header.Add("Content-Type", "application/json")
-log.Print(data, data["data"], notification, notification["notification"], req);
-log.Print(data["data"]);
+log.Print(data);
 log.Print(notification);
-log.Print(notification["notification"], req);
 log.Print(req);
 	resp, err := s.Http.Do(req)
 	if err != nil {
